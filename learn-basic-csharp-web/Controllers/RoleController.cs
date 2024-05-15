34ecfe10364936abbab2ad8e1d4ffdf2a7ae013d
mogam99 <mogams1811@gmail.com>
2024-05-15 17:06:09 +0700

    feat create, update, delete on roles page

---
+++ learn-basic-csharp-web/Controllers/RoleController.cs
@@ -0,0 +1,115 @@
+﻿using learn_basic_csharp_web.Models;
+using learn_basic_csharp_web.Models.EF;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace learn_basic_csharp_web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ILogger<RoleController> _logger;
+        private readonly ModelContext _context;
+
+        public RoleController(ILogger<RoleController> logger, ModelContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new Models.RoleVM.Index();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var response = new ResponseBase();
+            try
+            {
+                var model = new Models.RoleVM.Add();
+                return PartialView("_Add", model);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+                return Json(response);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Models.RoleVM.Add input)
+        {
+            var response = new ResponseBase();
+            try
+            {
+                var save = await Models.RoleVM.Save(input);
+                response.Status = true;
+                response.Message = "Data berhasil dibuat.";
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+            }
+            return Json(response);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var response = new ResponseBase();
+            try
+            {
+                var model = new Models.RoleVM.Edit(Convert.ToInt32(id));
+                return PartialView("_Edit", model);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+                return Json(response);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.RoleVM.Edit input)
+        {
+            var response = new ResponseBase();
+            try
+            {
+                var update = await Models.RoleVM.Update(input);
+                response.Status = true;
+                response.Message = "Data berhasil diubah.";
+
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+            }
+            return Json(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Models.RoleVM.Delete input)
+        {
+            var response = new ResponseBase();
+            try
+            {
+                var delete = await Models.RoleVM.Destroy(input);
+                response.Status = true;
+                response.Message = "Data berhasil dihapus.";
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+            }
+            return Json(response);
+        }
+    }
+}
--- learn-basic-csharp-web/Models/RoleVM.cs
+++ learn-basic-csharp-web/Models/RoleVM.cs
@@ -1,9 +1,91 @@
 ﻿using learn_basic_csharp_web.Models.EF;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
 
 namespace learn_basic_csharp_web.Models
 {
     public class RoleVM
     {
-        public required List <Role> Roles { get; set; }
+        public class Index
+        {
+            public List<Role> Roles { get; set; } = new List<Role>();
+            public Index()
+            {
+                var context = new ModelContext();
+                Roles = context.Roles.ToList();
+            }
+        }
+
+        public class Add
+        {
+            public Role Role { get; set; } = new Role();
+            public Add() { }
+        }
+
+        public class Edit
+        {
+            public int Id { get; set; }
+            public Role Role { get; set; } = new Role();
+            public Edit() { }
+            public Edit(int id)
+            {
+                var context = new ModelContext();
+                var query = context.Roles.FirstOrDefault(x => x.Id == id);
+                if (query != null) Role = query;
+            }
+        }
+
+        public class Delete
+        {
+            public required int Id { get; set; }
+        }
+
+        public static async Task<bool> Update(Edit input)
+        {
+            bool result = false;
+            var context = new ModelContext();
+            var query = await context.Roles.FirstOrDefaultAsync(x => x.Id == input.Id);
+
+            if (query == null) throw new Exception("Data not found.");
+            if (string.IsNullOrEmpty(input.Role.Name)) throw new Exception("Role name is required.");
+
+            query.Name = input.Role.Name;
+            await context.SaveChangesAsync();
+
+            return result;
+        }
+
+        public static async Task<bool> Save(Add input)
+        {
+            bool result = false;
+            var context = new ModelContext();
+
+            if (string.IsNullOrEmpty(input.Role.Name)) throw new Exception("Role name is required.");
+
+            var _add = new EF.Role();
+            _add.Name = input.Role.Name;
+
+            context.Roles.Add(_add);
+            await context.SaveChangesAsync();
+
+            return result;
+        }
+
+        public static async Task<bool> Destroy(Delete input)
+        {
+            using (var context = new ModelContext())
+            {
+                var query = await context.Roles.FirstOrDefaultAsync(x => x.Id == input.Id);
+
+                if (query == null) throw new Exception("Data not found.");
+
+                context.Roles.Remove(query);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+
+        }
     }
+
 }
