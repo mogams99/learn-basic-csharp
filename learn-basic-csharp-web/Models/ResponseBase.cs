dd9327473beb037952c2b6dfef18c12e1014c256
mogam99 <mogams1811@gmail.com>
2024-05-15 17:06:22 +0700

    create response base

---
+++ learn-basic-csharp-web/Models/ResponseBase.cs
@@ -0,0 +1,8 @@
+﻿namespace learn_basic_csharp_web.Models
+{
+    public class ResponseBase
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
